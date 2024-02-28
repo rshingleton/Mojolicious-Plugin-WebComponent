7e3d183bdfa5c6802b61dec2f74273d1fd3312d3
shingler <shingler@oclc.org>
2024-02-28 14:54:47 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# NAME
+
+Mojolicious::Plugin::WebComponent - It's new $module
+
+# SYNOPSIS
+
+    use Mojolicious::Plugin::WebComponent;
+
+# DESCRIPTION
+
+Mojolicious::Plugin::WebComponent is ...
+
+# LICENSE
+
+Copyright (C) shingler.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+shingler <shingler@oclc.org>
+# Mojolicious-Plugin-WebComponent
---
+++ lib/Mojolicious/Plugin/WebComponent.pm
@@ -0,0 +1,39 @@
+package Mojolicious::Plugin::WebComponent;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojolicious::Plugin::WebComponent - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mojolicious::Plugin::WebComponent;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::WebComponent is ...
+
+=head1 LICENSE
+
+Copyright (C) shingler.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+shingler E<lt>shingler@oclc.orgE<gt>
+
+=cut
+
